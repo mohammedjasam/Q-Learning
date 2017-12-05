8c6e3eb2e57503edf5ea76bb963d684ab66573ed
Mohammed Jasam <mnqnd@mst.edu>
2017-12-05 01:07:22 -0600

    Stuck with the simulation of falling donuts!

--- GridWorld/GridWorld/World.cpp
+++ GridWorld/GridWorld/World.cpp
@@ -3,6 +3,9 @@
 #include<iomanip>
 #include<map>
 #include<conio.h>
+#include <time.h>
+#include <thread>         // std::this_thread::sleep_for
+#include <chrono>
 
 using namespace std;
 
@@ -17,30 +20,125 @@ location donut[4];
 
 location Tim;
 
+int PlaceDonut(char base[10][10])
+{
+	srand(time(NULL));
+	cout << "         IN PLACE DONUT FUNCTION    "<<endl<< "There is a donut: " << IS_THERE_DONUT_ON_MAP;
+
+	if (IS_THERE_DONUT_ON_MAP != true)
+	{
+		double place_donut = rand() % 100;
+		int the_corner = rand() % 4;
+		cout <<"    The prob of donut is: " <<place_donut;
+
+		if (place_donut < 25)
+		{
+			cout << "  placed in Corner: " << the_corner << endl;
+			switch (the_corner)
+			{
+			case 0:
+				base[1][1] = 'D';
+				IS_THERE_DONUT_ON_MAP = true;
+				return 0;
+				break;
+			case 1:
+				base[1][8] = 'D';
+				IS_THERE_DONUT_ON_MAP = true;
+				return 1;
+				break;
+			case 2:
+				base[8][1] = 'D';
+				IS_THERE_DONUT_ON_MAP = true;
+				return 2;
+				break;
+			case 3:
+				base[8][8] = 'D';
+				IS_THERE_DONUT_ON_MAP = true;
+				return 3;
+				break;
+			}
+		}
+		else
+		{
+			//cout <<endl<< "No donut for Tim today prob was: " << place_donut << endl;
+			return -1;
+		}
+	}
+}
 
 
 void dropTim(location Tim)
 {
 	cout << endl << "DROPPING TIMMMMMMM!!!!!!!" << endl;
 	base[Tim.x][Tim.y] = 'R';
-
 }
 
 void renderWorld(location moved)
 {
+	int loc = PlaceDonut(base);
+
+	
+
 	base[Tim.x][Tim.y] = ' ';
+
 	location recall = Tim;
-	if (base[moved.x][moved.y] != 'W')
-	{
+
+	if (base[moved.x][moved.y] != 'X')
+	{		
 		base[moved.x][moved.y] = 'R';
 		Tim.x = moved.x;
 		Tim.y = moved.y;
+		cout <<endl<< "Tims location " << Tim.x <<", " <<Tim.y<<endl;
+
+
+		/*if (loc == 0)
+		{
+			if (base[1][1] == 'D' && Tim.x == 1 && Tim.y == 1)
+			{
+				IS_THERE_DONUT_ON_MAP = false;
+			}
+		}
+		else if (loc == 1)
+		{
+			if (base[1][8] == 'D' && Tim.x == 1 && Tim.y == 8)
+			{
+				IS_THERE_DONUT_ON_MAP = false;
+			}
+		}
+		else if (loc == 2)
+		{
+			if (base[8][1] == 'D' && Tim.x == 8 && Tim.y == 1)
+			{
+				IS_THERE_DONUT_ON_MAP = false;
+			}
+			else if (base[8][1] == ' ')
+			{
+				IS_THERE_DONUT_ON_MAP = false;
+			}
+		}
+		else if (loc == 3)
+		{
+			if (base[8][8] == 'D' && Tim.x == 8 && Tim.y == 8)
+			{
+				IS_THERE_DONUT_ON_MAP = false;
+			}
+			else if (base[8][8] == ' ')
+			{
+				IS_THERE_DONUT_ON_MAP = false;
+			}
+		}*/
+
+
 	}
 	else
 	{
 		base[recall.x][recall.y] = 'R';
 	}
 
+	if (((Tim.x == 1 && Tim.y == 1) && IS_THERE_DONUT_ON_MAP == true && loc == 0) || ((Tim.x == 1 && Tim.y == 8) && IS_THERE_DONUT_ON_MAP == true && loc == 1) || ((Tim.x == 8 && Tim.y == 1) && IS_THERE_DONUT_ON_MAP == true && loc == 2) || ((Tim.x == 8 && Tim.y == 8) && IS_THERE_DONUT_ON_MAP == true && loc == 3))
+	{
+		IS_THERE_DONUT_ON_MAP = false;
+	}
 
 
 	cout << endl << char(201) << char(205) << char(205) << char(205) << char(205) << char(205) << char(203) << char(205) << char(205) << char(205) << char(205) << char(205) << char(203) << char(205) << char(205) << char(205) << char(205) << char(205) << char(203) << char(205) << char(205) << char(205) << char(205) << char(205) << char(203) << char(205) << char(205) << char(205) << char(205) << char(205) << char(203) << char(205) << char(205) << char(205) << char(205) << char(205) << char(203) << char(205) << char(205) << char(205) << char(205) << char(205) << char(203) << char(205) << char(205) << char(205) << char(205) << char(205) << char(203) << char(205) << char(205) << char(205) << char(205) << char(205) << char(203) << char(205) << char(205) << char(205) << char(205) << char(205) << char(187) << endl;
@@ -69,46 +167,7 @@ void renderWorld(location moved)
 	}
 }
 
-void PlaceDonut(char base[10][10])
-{
-	srand(time(NULL));
-	//cout << "IN PLACE DONUT FUNCTION";
 
-	if (IS_THERE_DONUT_ON_MAP != true)
-	{
-		double place_donut = rand() % 100;
-		int the_corner = rand() % 4;
-
-
-		if (place_donut < 25)
-		{
-			cout << "Placing Donut in Corner: " << the_corner<<endl;
-			switch (the_corner)
-			{
-			case 0:
-				base[1][1] = 'D';
-				IS_THERE_DONUT_ON_MAP = true;
-				break;
-			case 1:
-				base[1][8] = 'D';
-				IS_THERE_DONUT_ON_MAP = true;
-				break;
-			case 2:
-				base[8][1] = 'D';
-				IS_THERE_DONUT_ON_MAP = true;
-				break;
-			case 3:
-				base[8][8] = 'D';
-				IS_THERE_DONUT_ON_MAP = true;
-				break;
-			}
-		}
-		else
-		{
-			cout << "No donut for Tim today prob was: " << place_donut<<endl;
-		}
-	}
-}
 
 
 
@@ -116,11 +175,12 @@ struct direction
 {
 	int x;
 };
+
 void try_move(location Tim, int x, int y)
 {
 	Tim.x += x;
 	Tim.y += y;
-	cout << "IM IN TRY MOVE";
+	//cout << "IM IN TRY MOVE";
 
 	renderWorld(Tim);
 }
@@ -128,7 +188,7 @@ void try_move(location Tim, int x, int y)
 void move_up(location Tim)
 {
 	try_move(Tim, 0, -1);
-	cout << "IM IN MOVE";
+	//cout << "IM IN MOVE";
 }
 
 void move_down(location Tim)
@@ -169,95 +229,87 @@ void World()
 
 
 	// Place walls
-	base[0][0] = 'W';
-	base[0][1] = 'W';
-	base[0][2] = 'W';
-	base[0][3] = 'W';
-	base[0][4] = 'W';
-	base[0][5] = 'W';
-	base[0][6] = 'W';
-	base[0][7] = 'W';
-	base[0][8] = 'W';
-	base[0][9] = 'W';
+	base[0][0] = 'X';
+	base[0][1] = 'X';
+	base[0][2] = 'X';
+	base[0][3] = 'X';
+	base[0][4] = 'X';
+	base[0][5] = 'X';
+	base[0][6] = 'X';
+	base[0][7] = 'X';
+	base[0][8] = 'X';
+	base[0][9] = 'X';
 
-	base[9][0] = 'W';
-	base[9][1] = 'W';
-	base[9][2] = 'W';
-	base[9][3] = 'W';
-	base[9][4] = 'W';
-	base[9][5] = 'W';
-	base[9][6] = 'W';
-	base[9][7] = 'W';
-	base[9][8] = 'W';
-	base[9][9] = 'W';
+	base[9][0] = 'X';
+	base[9][1] = 'X';
+	base[9][2] = 'X';
+	base[9][3] = 'X';
+	base[9][4] = 'X';
+	base[9][5] = 'X';
+	base[9][6] = 'X';
+	base[9][7] = 'X';
+	base[9][8] = 'X';
+	base[9][9] = 'X';
 
-	base[1][0] = 'W';
-	base[2][0] = 'W';
-	base[3][0] = 'W';
-	base[4][0] = 'W';
-	base[5][0] = 'W';
-	base[6][0] = 'W';
-	base[7][0] = 'W';
-	base[8][0] = 'W';
+	base[1][0] = 'X';
+	base[2][0] = 'X';
+	base[3][0] = 'X';
+	base[4][0] = 'X';
+	base[5][0] = 'X';
+	base[6][0] = 'X';
+	base[7][0] = 'X';
+	base[8][0] = 'X';
 
-	base[1][9] = 'W';
-	base[2][9] = 'W';
-	base[3][9] = 'W';
-	base[4][9] = 'W';
-	base[5][9] = 'W';
-	base[6][9] = 'W';
-	base[7][9] = 'W';
-	base[8][9] = 'W';
+	base[1][9] = 'X';
+	base[2][9] = 'X';
+	base[3][9] = 'X';
+	base[4][9] = 'X';
+	base[5][9] = 'X';
+	base[6][9] = 'X';
+	base[7][9] = 'X';
+	base[8][9] = 'X';
 
-	base[6][4] = 'W';
-	base[7][4] = 'W';
-	base[8][4] = 'W';
+	base[6][4] = 'X';
+	base[7][4] = 'X';
+	base[8][4] = 'X';
 
-	base[4][6] = 'W';
-	base[5][6] = 'W';
-	base[6][6] = 'W';
+	base[4][6] = 'X';
+	base[5][6] = 'X';
+	base[6][6] = 'X';
 
-	base[2][4] = 'W';
-	base[2][5] = 'W';
-	base[2][6] = 'W';
+	base[2][4] = 'X';
+	base[2][5] = 'X';
+	base[2][6] = 'X';
 
-	base[2][2] = 'W';
-	base[3][2] = 'W';
-	base[4][2] = 'W';
+	base[2][2] = 'X';
+	base[3][2] = 'X';
+	base[4][2] = 'X';
 
-	PlaceDonut(base);
+	
 	dropTim(Tim);
 	//cout << setw(10);
 
 	renderWorld(Tim);
 
-	int event = -1;
-
-	while (event != 27)
+	int event = -1, i = 0;
+	
+	while (i < 100)
 	{
-		cin >> event ;
-		cout << event;
+		//event = rand() % 4;
+		
+		//if (event == 0) { move_up(Tim); }
+		//else if (event == 1) { move_down(Tim); }
+		//else if (event == 2) { move_right(Tim); }
+		//else if (event == 3) { move_left(Tim); }
+		//std::this_thread::sleep_for(std::chrono::seconds(1));
+
+
+		cin >> event;
 
 		if (event == 4) { move_up(Tim); }
 		else if (event == 6) { move_down(Tim); }
 		else if (event == 2) { move_right(Tim); }
 		else if (event == 8) { move_left(Tim); }
 
-		//switch(event)
-		//{
-		//	case 22472:
-		//		cout << "IM IN SWITCH";
-		//		move_up(Tim);
-		//		break;
-		//	case 22480:
-		//		move_down(Tim);
-		//		break;
-		//	case 22477:
-		//		move_right(Tim);
-		//		break;
-		//	case 22475:
-		//		move_left(Tim);
-		//		break;
-		//}
 	}
 }
