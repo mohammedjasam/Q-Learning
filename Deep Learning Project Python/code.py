2d29821d6360fc24702affc0b8ca0b836bf85e05
Mohammed Jasam <mnqnd@mst.edu>
2017-12-09 23:44:44 -0600

    Building the Map

---
+++ Deep Learning Project Python/code.py
@@ -0,0 +1,47 @@
+import random
+
+
+"""__________________GLOBAL VARIABLES__________________"""
+
+# Q-Learning
+Alpha = 0.1 # Learning rate (FIXED NOW)
+DiscountFactor = 0.1 # Considers discounted rewards after time t
+States = 100
+Actions = 4
+Q = [] # 2D Array to store States and their Actions
+
+# Map
+H = 10 # Height of the Map
+W = 10 # Width of the Map
+Map = [] # The 2D array to store the Map of the basement
+DonutPresentOnMap = False # Bool value to keep track of the presence of donut
+
+# Tinny Tim
+X = 4 # Initial X Coordinate
+Y = 4 # Initial Y Coordinate
+
+
+"""__________________INITIALIZATIONS___________________"""
+
+
+
+
+
+
+def Q_Learning():
+
+
+
+
+def main():
+
+
+
+
+
+
+
+
+
+""" THIS IS WHERE THE EXECUTION BEGINDS """
+main() # Calls  the main function which runs the whole program!
